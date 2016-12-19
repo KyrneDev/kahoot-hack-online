4bac2fe047f75f6336f53c26a354616fdd00f281
Elliot Gluck <elliotgluck@gmail.com>
2016-12-19 15:50:43 -0800

    12/19/16 Site Redesign
    +Redesigned website
    +Fixed modal
    -Removed unnecessary clutter

--- assets/js/countdown.js
+++
@@ -1,8 +0,0 @@
-function countdown() {
-    var i = document.getElementById('counter');
-    if (parseInt(i.innerHTML)<=0) {
-        location.href = 'index.html';
-    }
-    i.innerHTML = parseInt(i.innerHTML)-1;
-}
-setInterval(function(){ countdown(); },1000);
---
+++ verify.js
@@ -0,0 +1,3 @@
+function modalFunc() {
+swal("Flood Request Sent!", "Look up, it's about to get a lot more crowded in here...", "success");
+}
